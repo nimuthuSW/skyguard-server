ce3c4ec83d4fdca5ae29182739f798ab5f98b1cf
DinukaD-24 <dinukailangakoon@gmail.com>
2026-07-22 16:05:04 +0530

    feat: initialize Prisma ORM with PostgreSQL adapter and implement user authentication controller

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import dotenv from 'dotenv';
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL as string });
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
